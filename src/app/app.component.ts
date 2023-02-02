3cb159ff4763fcc4f75fc288b985e2ad392906c8
unknown <1902343@utrivieramaya.edu.mx>
2023-02-02 13:38:31 -0500

    Versión del diseño listo, analizar si implementar colores en modo white

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {}
+
+  checkDarkTheme() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+    if ( prefersDark.matches ) {
+      document.body.classList.toggle( 'dark' );
+    }
+  }
 }
--- src/app/config/tab3.page.ts
+++ src/app/config/tab3.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +7,22 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  darkMode: boolean = true;
+
+  constructor(private render: Renderer2) {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+    this.darkMode = prefersDark.matches;
+  }
+
+
+  toggleTheme(event){
+    if (event.detail.checked){
+      //document.body.setAttribute('color-theme', 'dark')
+      this.render.setAttribute(document.body, 'color-theme', 'dark')
+    }else{
+      //document.body.setAttribute('color-theme', 'light')
+      this.render.setAttribute(document.body, 'color-theme', 'light')
+    }
+  }
 
 }
